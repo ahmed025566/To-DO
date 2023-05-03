a8670e0fe158688be9329c7f6bb15b37bb76c0ea
ahmed025566 <ahmedsalah025566@gmail.com>
2023-05-03 21:10:33 +0200

    Add my app

--- src/index.js
+++ src/index.js
@@ -1,20 +1,25 @@
 import './index.css';
 
 const tasksDiv = document.querySelector('.tasksDiv');
-const arrayOfTasks = [
-  {
-    description: 'wash the dishes',
-    id: Date.now(),
-    completed: false,
-    index: 1,
-  },
-  {
-    description: 'Complete to do app project',
-    id: Date.now(),
-    completed: false,
-    index: 2,
-  },
-];
+const form = document.querySelector('.form');
+const text = document.querySelector('.task-text');
+
+const saveToStorage = (arrayOfTasks) => { 
+  for (let i = 0, j = 1; i < arrayOfTasks.length; i += 1, j += 1) {
+    
+    arrayOfTasks[i].IDX = j;
+  }
+  localStorage.setItem('tasks', JSON.stringify(arrayOfTasks));
+};
+
+
+const getFromStorage = () => {
+  const storage = localStorage.getItem('tasks') === null
+    ? [] : JSON.parse(localStorage.getItem('tasks'));
+  return storage;
+};
+
+let arrayOfTasks = getFromStorage();
 
 const displayData = () => {
   tasksDiv.innerHTML = '';
@@ -23,12 +28,98 @@ const displayData = () => {
     div.className = 'task';
     div.setAttribute('data-id', task.id);
     div.innerHTML = '<i class="fa-regular fa-square"></i>';
-    div.innerHTML += `<input type="text" class = "taskText" readonly value="${task.description}">`;
-    div.innerHTML += '<i class="fa-solid fa-ellipsis-vertical"></i>';
+    div.innerHTML += `<input type="text" class = "taskText" value="${task.description}">`;
+    
+     div.innerHTML += '<i class="fa-solid fa-trash-can"></i>';
     tasksDiv.append(div);
   });
 };
 
-window.addEventListener('DOMContentLoaded', () => {
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const task = {
+    id: Date.now(),
+    description: text.value,
+    completed: false,
+    IDX: arrayOfTasks.length + 1,
+  };
+  arrayOfTasks.push(task);
+  text.value = '';
+  saveToStorage(arrayOfTasks);
   displayData();
 });
+const edit = () => {
+  tasksDiv.addEventListener('input', (e) => {
+    for (let i = 0; i < arrayOfTasks.length; i += 1) {
+      if (arrayOfTasks[i].id === +e.target.parentElement.getAttribute('data-id')) {
+        arrayOfTasks[i].description = e.target.value;
+        saveToStorage(arrayOfTasks);
+      }
+    }
+  });
+};
+
+const removeWithId = (taskID) => {
+  arrayOfTasks = arrayOfTasks.filter((task) =>  task.id !== +taskID );
+  saveToStorage(arrayOfTasks);
+}
+
+const deleteTask = () => {
+  tasksDiv.addEventListener('click', (e) => {
+    if(e.target.classList.contains('fa-trash-can')) {
+       removeWithId(e.target.parentElement.getAttribute('data-id'))
+      e.target.parentElement.remove();
+    }
+  })
+}
+const changeStyle = () => {
+  tasksDiv.addEventListener('click', (e) => {
+    for (let i = 0; i < arrayOfTasks.length; i += 1) {
+      if (+e.target.parentElement.getAttribute('data-id') === arrayOfTasks[i].id) {
+        e.target.style.background = '#FFF9A6'
+        tasksDiv.childNodes[i].style.background = '#FFF9A6';
+        
+      }
+    }
+  });
+
+}
+const resetStyle = () => {
+  tasksDiv.addEventListener('click', (e) => {
+    for (let i = 0; i < arrayOfTasks.length; i += 1) {
+      if (arrayOfTasks[i].id !== +e.target.parentElement.getAttribute('data-id')) {
+        for (let k = 0; k < tasksDiv.childNodes.length; k += 1) {
+          tasksDiv.childNodes[i].style.background = '#fff';
+          tasksDiv.childNodes[i].childNodes[1].style.background = '#fff';
+          
+        }
+      }
+    }
+  })
+}
+const reset = () => {
+  document.addEventListener('click', (e) => {
+    if (!e.target.classList.contains('taskText')) {
+      for (let i = 0; i < tasksDiv.childNodes.length; i += 1) {
+        tasksDiv.childNodes[i].style.background = '#fff';
+        tasksDiv.childNodes[i].childNodes[1].style.background = '#fff';
+       
+        
+      }
+    }
+  })
+}
+
+
+window.addEventListener('DOMContentLoaded', () => {
+  displayData();
+  edit();
+  reset();
+  resetStyle();
+  changeStyle();
+  deleteTask()
+});
+
+const clear = () => {
+  localStorage.setItem('tasks', JSON.stringify([]));
+};
